a42c0c826a8c8eaae05badc4e87ece6cca2ce585
Kevin Chung <kevin@chun.gd>
2015-08-16 14:58:38 -0700

    Add cross-realm openings test stub

---
+++ test/crossrealm-openings.js
@@ -0,0 +1,11 @@
+import { expect } from 'chai';
+import util from './util';
+
+describe('Cross-realm API', () => {
+  describe('fetch', () => {
+
+    // No tests are created since my account doesn't utilize this yet :(
+    it.skip('test should be implemented');
+
+  });
+});
